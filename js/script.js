95a72f2c557ae989e74379a1e29d130d1ba095ae
FontaMatte <matteo.fontanel@gmail.com>
2023-01-31 15:35:13 +0100

    Basic exercise completed

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        
+        return {
+            
+            mailList : []
+        }
+    },
+
+    methods: {
+
+    },
+
+    mounted() {
+
+        for (i = 0; i < 10; i++) {
+
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+
+                    this.mailList.push(response.data.response);
+
+                });
+        }
+    }
+}).mount('#app');
